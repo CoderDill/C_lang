c48b9e96df1634d765e6b6f5ad6dfb0ffeab5f5a
Matthew Dillon <mddillon47@gmail.com>
2024-01-15 17:24:44 -0800

    first C programs

---
+++ poem.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    printf("To keep your marriage brimming\n"
+           "With love in the loving cup,\n"
+           "Whenever you’re wrong, admit it;\n"
+           "Whenever you’re right, shut up.\n");
+    return 0;
+}
